fc54c56346026767bb761b4d5b457f909059ffab
[01;31m[K"name"[m[K: "Wyeth Jackson <[01;31m[K"email"[m[K: "wyethjackson@gmail.com>
2015-10-09 18:50:22 -0500

    location filtering spike

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,25 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+	var railsEnv = $('body').data('env')
+	$("#location").on("click", function(){
+		$.ajax(
+			url: "https://www.googleapis.com/geolocation/v1/geolocate?key=" + railsEnv,
+			type: "POST",
+			success: function(response) {
+				console.log(reponse)
+			})
+	})
+	// window.onload = function() {
+	//   var startPos;
+	//   var geoSuccess = function(position) {
+	//     startPos = position;
+	//     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+	//     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+	//   };
+	//   navigator.geolocation.getCurrentPosition(geoSuccess);
+	// };
+
+})
