531545bc8f9021419720e1394b3cad74ef9d19ff
[01;31m[K"name"[m[K: "Wyeth Jackson <[01;31m[K"email"[m[K: "wyethjackson@gmail.com>
2015-10-10 08:32:20 -0500

    google API

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,13 +31,34 @@ $(document).ready(function(){
   	})
 
 })
-	
 
-//  function initMap() {
-//   // var myLatlng = {lat: -25.363, lng: 131.044};
+function CoordMapType(tileSize) {
+  this.tileSize = tileSize;
+}
 
+CoordMapType.prototype.getTile = function(coord, zoom, ownerDocument) {
+  var div = ownerDocument.createElement('div');
+  div.innerHTML = coord;
+  div.style.width = this.tileSize.width + 'px';
+  div.style.height = this.tileSize.height + 'px';
+  div.style.fontSize = '10';
+  div.style.borderStyle = 'solid';
+  div.style.borderWidth = '1px';
+  div.style.borderColor = '#AAAAAA';
+  return div;
+};
 
-// }
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: {lat: 41.850, lng: -87.650}
+  });
 
+  // Insert this overlay map type as the first overlay map type at
+  // position 0. Note that all overlay map types appear on top of
+  // their parent base map.
+  map.overlayMapTypes.insertAt(
+      0, new CoordMapType(new google.maps.Size(256, 256)));
+}
  
 
