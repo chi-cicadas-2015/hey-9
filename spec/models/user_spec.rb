e2929b6885920e4919ab19beffa44d34ece3e536
trodicaro <trodicaro@gmail.com>
2015-10-10 10:48:48 -0500

    Set up for testing
    Gems for rspec-rails, capybara, factory_girl, shoulda-matchers, faker. Also set keys in secrets.yml for test, development and production environments.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'create' do
+
+    # it 'should create a new user' do
+    #   expect { user.create(:name => "Mitch", :password => "secret") }.to change(User, :count).by(1)
+    # end
+
+    # it 'should require a name >= 4 characters' do
+    #   user.create(:name => "No", :password => "secret").should_not be_valid
+    # end
+
+    # it 'should verify the uniqueness of the name' do
+    
+    # end
+
+  end
+
 end
