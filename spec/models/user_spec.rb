c696d38a329bdb00319e1eca5d3e1f5fea1a8938
[01;31m[K"name"[m[K: "Wyeth Jackson <[01;31m[K"email"[m[K: "wyethjackson@gmail.com>
2015-10-10 11:21:15 -0500

    user.dogs rspec test completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe 'associations' do 
+		before {
+			@user = User.create!(username: "Wyeth", email: "wyeth@wyeth", password_digest: "123")
+			@dog = Dog.create!(owner_id: @user.id)
+			@event = Event.create!(creator_id: @user.id)
+			@comment = Comment.create!(commenter_id: @user.id)
+			@direct_conversation = DirectConversation.create!(subject: "what up")
+			@conversing = Conversing.create!(user_id: @user.id, conversation_id: @direct_conversation.id)
+		}
+		it 'should return the dogs related to this user' do 
+			expect(@user.dogs).to eq([@dog])
+		end
+ 	end
 end
