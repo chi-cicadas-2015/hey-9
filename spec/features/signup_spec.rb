65b21a759c54ba67436519fd5761b3f6710c435f
trodicaro <trodicaro@gmail.com>
2015-10-10 11:21:34 -0500

    Set up for testing
    Gems for rspec-rails, capybara, factory_girl, shoulda-matchers, faker. Also
    set keys in secrets.yml for test, development and production environments.

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature "User signup" do
   scenario "when user finishes sign up" do
-    visit '/'
+    # visit '/'
 
     # allow_any_instance_of(Game).to receive(:computer_throw) { 'rocket' }
     # first(".btn-success").click
