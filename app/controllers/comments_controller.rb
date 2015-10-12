9fe47c82053115da81301e7783f14520120c2331
MsLarissaaa <larissapittenger@gmail.com>
2015-10-12 11:37:51 -0500

    Add polymorphic comments functionality and partials

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,9 @@ class CommentsController < ApplicationController
 
   def create
     @comment = @commentable.comments.new comment_params
-    @comment.user = current_user
+    @comment.commenter = current_user
     @comment.save
-    redirect_to @commentable, notice: "Your comment was successfully posted."
+    redirect_to :back, notice: "Your comment was successfully posted."
   end
 
   private
@@ -16,38 +16,6 @@ class CommentsController < ApplicationController
   end
 end
 
-
-  # def new
-  #   @comment = Comment.new
-  #   respond_to do |format|
-  #       format.html { redirect_to film_path(@film) }
-  #       format.js
-  #   end
-  # end
-
-  # def create
-  #   @message = Message.find(params[:])
-  #   @comment= Comment.new(params)
-  #   @comment.commenter_id = current_user.id
-  #   if @comment.save
-  #   end
-  # end
-
-  # def create
-  #   @film = Film.find(params[:film_id])
-  #   @review = @film.reviews.find(params[:review_id])
-  #   @comment = @review.comments.build(comment_params)
-
-  #   if @comment.save
-  #     respond_to do |format|
-  #         format.html { redirect_to film_path(@film) }
-  #         format.js
-  #     end
-  #   else
-  #     head 500
-  #   end
-
-  # end
 
 
 
