7130a5e1dab216fe211e8d347d53fee48fd5d747
Alexis Moody <amoodyldes@gmail.com>
2015-10-12 13:51:36 -0500

    start adding links for a more streamlined user experience

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
-
+  include ApplicationHelper
   def index
-
+    if current_user
+      redirect_to "/users/#{current_user.id}"
+    else
+      render :'home/index', :layout => false
+    end
   end
 
 end
