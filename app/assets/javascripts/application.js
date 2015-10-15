b2df63df14b5c8edec5fb824296a40d1719dee29
Wyeth Jackson <wyethjackson@gmail.com>
2015-10-15 09:17:43 -0500

    clean up application.js code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,26 +28,7 @@ $(document).ready(function(){
   changeWeather("#weather-card");
   changeWeather("#weather-card2");
 
-  // $("#location_sync").on("click", function(event){
-  //   event.preventDefault();
 
-  //   $.ajax({
-  //     method: "POST",
-  //     url: "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCxHSsHvEIld84HHYj5nvetfW6cV7gYNAs",
-  //     success: function(response) {
-  //       console.log(response)
-
-  //       $.ajax({
-  //         method: "POST",
-  //         url: "/sessions/sync_location",
-  //         data: response,
-  //         success: function(response) {
-
-  //           }
-  //         })
-  //       }
-  //     })
-  //   })
 
   $("#add-friend").on("click", function(event){
     event.preventDefault();
@@ -61,11 +42,7 @@ $(document).ready(function(){
       }
     })
   })
-  // $("#pet-stores").on("click", function(){
-  //   console.log("whatup")
 
-
-  // });
 
 
   // http://api.jquery.com/jQuery.noConflict/
@@ -83,30 +60,7 @@ $(document).ready(function(){
   //     changeMonth: true,
   //   });
 
-  // https://github.com/jonthornton/jquery-timepicker
-  // $("#event-button").on("click", function(){
-  //      var eventLocation = $("#event-map").attr('name');
-  //      console.log('yooooooo')
-  //     var lat
-  //     var lng
-  //     $.ajax({
-  //       method: "POST",
-  //       dataType: 'json',
-  //       url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + eventLocation + "&key=AIzaSyDj8b8ELTA9Zq9pW7IY1L4TSUX0PClr06M",
-  //       success: function(response) {
-  //         lat = response.results[0].geometry.location.lat
-  //         lng = response.results[0].geometry.location.lng
-  //       $.ajax({
-  //         method: "POST",
-  //         url: "/events",
-  //         data: response,
-  //         success: function(response) {
-  //             console.log(response)
-  //           }
-  //         })
-  //       }
-  //     })
-  // })
+
 
 })
 
